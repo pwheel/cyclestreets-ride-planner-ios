41242e37f66c2e7fd2e92b4ef3e94c94122cf500
Paul Wheeler <paul.william.wheeler@gmail.com>
2026-06-30 21:43:04 +0100

    Initial Commit

---
+++ CycleStreets Ride Planner/CycleStreets_Ride_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleStreets_Ride_PlannerApp.swift
+//  CycleStreets Ride Planner
+//
+//  Created by Paul Wheeler on 30/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CycleStreets_Ride_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycleStreets Ride PlannerTests/CycleStreets_Ride_PlannerTests.swift
@@ -0,0 +1,19 @@
+//
+//  CycleStreets_Ride_PlannerTests.swift
+//  CycleStreets Ride PlannerTests
+//
+//  Created by Paul Wheeler on 30/06/2026.
+//
+
+import Testing
+@testable import CycleStreets_Ride_Planner
+
+struct CycleStreets_Ride_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
